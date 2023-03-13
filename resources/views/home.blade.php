0f6029b1025144ee09316c88043c901ff500fb24
Brandon Vasquez <thatsjaskett@gmail.com>
2023-03-13 17:48:35 +0100

    Stampa su pagina elenco film

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,11 @@
 @extends('layouts.layout')
 
 @section('content')
-    <h1>Hello from content</h1>
+    <h1>Movie List</h1>
+
+    <ul>
+        @foreach ($movies as $movie)
+            <li>{{ $movie['title'] }}</li>
+        @endforeach
+    </ul>
 @endsection
