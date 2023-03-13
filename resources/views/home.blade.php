2487494f1d033263ef30eff0ab0865390f6fcc34
Brandon Vasquez <thatsjaskett@gmail.com>
2023-03-13 17:50:14 +0100

    Layout

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,11 +1,33 @@
 @extends('layouts.layout')
 
 @section('content')
-    <h1>Movie List</h1>
+    <div class="container">
+        <h1 class="my-4">Movies List</h1>
 
-    <ul>
-        @foreach ($movies as $movie)
-            <li>{{ $movie['title'] }}</li>
-        @endforeach
-    </ul>
+        <table class="table">
+            <thead>
+                <tr>
+                    <th>Id</th>
+                    <th>Title</th>
+                    <th>Original title</th>
+                    <th>Nationality</th>
+                    <th>Date</th>
+                    <th>Vote</th>
+                </tr>
+            </thead>
+
+            <tbody class="table-group-divider">
+                @foreach ($movies as $movie)
+                    <tr>
+                        <td>{{ $movie['id']}}</td>
+                        <td>{{ $movie['title']}}</td>
+                        <td>{{ $movie['original_title']}}</td>
+                        <td>{{ $movie['nationality']}}</td>
+                        <td>{{ $movie['date']}}</td>
+                        <td>{{ $movie['vote']}}</td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
 @endsection
