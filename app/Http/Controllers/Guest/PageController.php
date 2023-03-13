551d1abc94c923d01edffbad5376be9dc7dabefe
Brandon Vasquez <thatsjaskett@gmail.com>
2023-03-13 17:48:23 +0100

    Stamoa elenco film

--- app/Http/Controllers/Guest/PageController.php
+++ app/Http/Controllers/Guest/PageController.php
@@ -5,9 +5,17 @@ namespace App\Http\Controllers\Guest;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
+use App\Models\Movie;
+
 class PageController extends Controller
 {
     public function home() {
-        return view("pages.home");
+        $movies = Movie::all();
+        
+        $data = [
+            "movies" => $movies
+        ];
+
+        return view("pages.home", $data);
     }
 }
